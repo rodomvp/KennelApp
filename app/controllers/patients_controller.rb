768d9ba2f0ae7549aa03f21640a2a2350a9dd639
rgulle4 <rgulle4@lsu.edu>
2015-11-13 17:01:11 -0600

    GENERATE routes for User and Patient resources

---
+++ app/controllers/patients_controller.rb
@@ -0,0 +1,10 @@
+class PatientsController < ApplicationController
+  def new
+  end
+
+  def show
+  end
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :owners
+  resources :patients
+
   get 'static_pages/home'
   get 'static_pages/documentation'
 
