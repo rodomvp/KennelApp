f0a54cc7906821b7a0dcde727b10d23f5e6cfc56
rgulle4 <rgulle4@lsu.edu>
2015-11-13 10:54:45 -0600

    GENERATE simple User
    A User has attrs:
    - email      : string
    - first_name : string
    - last_name  : string

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151113155908) do
+ActiveRecord::Schema.define(version: 20151113165234) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,5 +36,13 @@ ActiveRecord::Schema.define(version: 20151113155908) do
   end
 
   add_index "patients", ["owner_id"], name: "index_patients_on_owner_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
