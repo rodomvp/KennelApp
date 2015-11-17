226dc51ad3b3c3c856f3a0c7c3304149c3b00106
rgulle4 <rgulle4@lsu.edu>
2015-11-17 01:04:57 -0600

    Make diagram script more complicated.
    - Also, update diagrams, bc model has changed a little since yesterday.

--- doc/make-diagrams-models.sh
+++ doc/make-diagrams-models.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 #railroady --hide-magic -M | dot -Tsvg > doc/diagrams-models.svg
 #railroady --hide-magic -a -i -M | dot -Tpng > doc/diagrams-models.png
-rake erd orientation=vertical filename=doc/diagrams-erd filetype=png
+function make_fancy_diagram {
+  rake erd orientation=vertical \
+           inheritance=true \
+           indirect=true \
+           polymorphism=true\
+           filename=doc/diagrams-erd filetype=$1
+}
+
+make_fancy_diagram png
+make_fancy_diagram pdf
 #rake erd orientation=vertical filename=doc/diagrams-erd filetype=svg
 #rake erd orientation=vertical filename=doc/diagrams-erd filetype=pdf
