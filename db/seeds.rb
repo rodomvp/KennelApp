2b09f11e77a5bd71cc0b5506ac6388d363b9d04a
vincent <vrodomista@gmail.com>
2015-11-17 03:56:20 -0600

    Fix merge conflicts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,23 @@
 Random.srand(1337) # seed the random number generator, for same results every time
 
 # Make owners and their pets
+
 patients_per_owner_distribution = [1,1,1,1,1,1,1,2,2,3] # most ppl have 1 pet
-<<<<<<< HEAD
 belongings_list = %w[Bone Treat Toy]
 diet_list = ["Mature", "Hypo-Allergenic Salmon", "Rabbit & Venison", "Owner's Food"]
 stay_year = 2015
 stay_minute = [0, 15, 30, 45]
 remarks_list = ["Stresses immensely upon arrival, use muzzle", "No legs", 
 "Only eats from owner's bowl", "Use holy water 2x per day on head"]
-=======
 species_list = %w[Cat, Dog, Hamster]
 belongings_list = %w[Bone, Treat, Toy]
->>>>>>> master
+
 50.times do
   the_new_owner = Owner.create(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
     phone_number: Faker::PhoneNumber.phone_number,
-<<<<<<< HEAD
-    email: "placeholder@fornow.com",
- 
-=======
     email: "placeholder@fornow.com"    
->>>>>>> master
   )
   the_new_owner.email = "owner_#{the_new_owner.id}@internet.com"
   the_new_owner.save
@@ -35,7 +29,6 @@ belongings_list = %w[Bone, Treat, Toy]
       weight: Faker::Name.first_name,      
       breed: Faker::Lorem.sentence(3),
       owner_id: the_new_owner.id,
-<<<<<<< HEAD
       belongings: belongings_list.sample,
       diet: diet_list.sample
     )
@@ -51,10 +44,7 @@ belongings_list = %w[Bone, Treat, Toy]
       random_day, random_month, random_minute),
     sch_check_out_dt: check_in + stay_length.days,
     patient_id: the_new_patient.id,
-    remarks: remarks_list.sample
-=======
-      belongings: belongings_list.sample
->>>>>>> master
+    remarks: remarks_list.sample,
     )
   end
 end
