729994472f11f7df257f6ee10e1600fa0a9d0a6a
rgulle4 <rgulle4@lsu.edu>
2015-11-18 07:22:39 -0600

    Add scheduled? and time_left methods to StayEvent
    - Just to add some meat to the model. Helps me understand things better.

--- app/models/stay_event.rb
+++ app/models/stay_event.rb
@@ -1,3 +1,13 @@
 class StayEvent < ActiveRecord::Base
   belongs_to :stay
+
+  def scheduled?
+    scheduled_time_stamp.present?
+  end
+
+  def time_left
+    if scheduled?
+      scheduled_time_stamp - DateTime.now
+    end
+  end
 end
