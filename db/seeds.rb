4beebcd67e862e2ca11eece8a18e808e2dbc0a66
rgulle4 <rgulle4@lsu.edu>
2015-11-21 13:06:05 -0600

    SEEDS: Add misc StayEvents to seeds iss #32
    - Add StayEvent and its subclasses to seeds.
    - This closes #32

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,43 +35,6 @@ breeds_list = [
   'Shetland Sheepdog'
 ]
 
-50.times do
-  the_new_owner = Owner.create(
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    phone_number: Faker::PhoneNumber.phone_number,
-    email: "placeholder@fornow.com"    
-  )
-  the_new_owner.email = "owner_#{the_new_owner.id}@internet.com"
-  the_new_owner.save
-
-  num_patients = patients_per_owner_distribution.sample
-  num_patients.times do
-  the_new_patient = Patient.create(
-      name: Faker::Name.first_name,
-      weight: Faker::Name.first_name,      
-      breed: breeds_list.sample,
-      owner_id: the_new_owner.id,
-      belongings: belongings_list.sample,
-      diet: diet_list.sample
-    )
-  
-  random_month = rand(1..12)
-  random_day = rand(1..28)
-  random_minute = stay_minute.sample
-  stay_length = rand(1..7)
-
-  Stay.create(
-    is_current: true,
-    sch_check_in_dt: check_in = DateTime.new(stay_year, random_month, 
-      random_day, random_month, random_minute),
-    sch_check_out_dt: check_in + stay_length.days,
-    patient_id: the_new_patient.id,
-    remarks: remarks_list.sample
-    )
-  end
-end
-
 # Creates Wards with Runns
 8.times do |i|
   the_new_ward = Ward.create(
@@ -81,14 +44,106 @@ end
   (i+2).times do |j|
     Runn.create(
       ident: ("Run %{i}.%{j}"),
-      size: ward_size.sample
-      )
+      size: ward_size.sample)
   end
 end
 
+50.times do
 
+  # Make owners
+  the_new_owner = Owner.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    phone_number: Faker::PhoneNumber.phone_number,
+    email: "placeholder@fornow.com"    
+  )
+  the_new_owner.email = "owner_#{the_new_owner.id}@internet.com"
+  the_new_owner.save
 
+  # Make patients
+  num_patients = patients_per_owner_distribution.sample
+  num_patients.times do
+    the_new_patient = Patient.create(
+        name: Faker::Name.first_name,
+        weight: Faker::Name.first_name,      
+        breed: breeds_list.sample,
+        owner_id: the_new_owner.id,
+        belongings: belongings_list.sample,
+        diet: diet_list.sample
+    )
 
+    # Make stays
+    has_stay = [true, false].sample
+    if has_stay 
+      random_month = rand(1..12)
+      random_day = rand(1..28)
+      random_minute = stay_minute.sample
+      stay_length = rand(1..7)
+
+      the_new_stay = Stay.create(
+        is_current: [true, false].sample,
+        sch_check_in_dt: check_in = DateTime.new(stay_year, random_month, 
+          random_day, random_month, random_minute),
+        sch_check_out_dt: check_in + stay_length.days,
+        patient_id: the_new_patient.id,
+        runn_id: Runn.all.sample.id, 
+        remarks: remarks_list.sample
+      )
+
+      sch_check_in_dt = the_new_stay.sch_check_in_dt
+      sch_check_out_dt = the_new_stay.sch_check_out_dt
+      if [true, false].sample
+        the_new_stay.check_out_dt = sch_check_out_dt
+      end
+      the_new_stay.save
+
+      # TODO: some stays have been checked in; this has to make sense
+      # against Time.current...
+      if [true, false].sample
+
+      end
+      
+      # make feeding_events < StayEvent
+      #some_time = sch_check_in_dt + 12.hours
+      #while some_time < sch_check_out_dt
+      some_time = sch_check_in_dt  
+      4.times do
+        some_time = some_time + 12.hours
+        feeding_event = FeedingEvent.create(
+          food_description: diet_list.sample,
+          did_eat: [true, false].sample,
+          remark: Faker::Lorem.sentence,
+          scheduled_time_stamp: some_time,
+          actual_time_stamp: some_time
+        )
+      end
+
+      # make ud_events < StayEvent
+      some_time = sch_check_in_dt
+      4.times do 
+        some_time = some_time + 6.hours
+        ud_event = UdEvent.create(
+          did_urinate: [true, true, true, true, true, false].sample,
+          did_defecate: [true, true, true, false].sample,
+          remark: Faker::Lorem.sentence,
+          scheduled_time_stamp: some_time,
+          actual_time_stamp: some_time
+        )
+      end
+      
+      # make misc_stay_event < StayEvent
+      some_time = sch_check_in_dt
+      (1..3).to_a.sample.times do
+        some_time = some_time + (1..8).to_a.sample
+        misc_stay_event = MiscStayEvent.create(
+          remark: Faker::Lorem.sentence,
+          scheduled_time_stamp: some_time,
+          actual_time_stamp: some_time,
+        )
+      end
+    end
+  end
+end
 
 # Make 2 users
 def create_user (a_first_name, a_last_name)
@@ -102,8 +157,5 @@ def create_user (a_first_name, a_last_name)
   )
 end
 
-create_user('George', 'Washignton')
+create_user('George', 'Washington')
 create_user('John', 'Adams')
-
-# TODO: seed some Runns and Wards, preferably hardcoded with typical
-# data... change the model if necessary.
