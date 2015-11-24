5be92c264b84c6fbb6498f20fd76697c77b3ab3e
vincent <vrodomista@gmail.com>
2015-11-24 04:11:10 -0600

    Add ability to login Change header for logged in/ logged out

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   resources :owners
   resources :patients
   resources :users
@@ -7,6 +9,9 @@ Rails.application.routes.draw do
   get 'docs' => 'static_pages#documentation'
   get 'new'  => 'users#new'
   get 'new_owner' => 'owners#new'
+  get 'login'=> 'sessions#new'
+  post 'login'=> 'sessions#create'
+  delete 'logout'=> 'sessions#destroy'
   # root 'patients#index'
   root 'static_pages#home'
 end
