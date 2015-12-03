a1284804978ea2408e3db9b155290a6ffb4f75b5
rgulle4 <rgulle4@lsu.edu>
2015-12-03 10:00:54 -0600

    Make ward view the home page.
     class StaticPagesController < ApplicationController
       def home
    -  	redirect_to all_patients_path if logged_in?
    +  	redirect_to ward_view_path if logged_in?
       end

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
-  	redirect_to all_patients_path if logged_in?		
+  	redirect_to ward_view_path if logged_in?
   end
 
   def documentation
