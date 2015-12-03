3e85a1dde1d2488df8599518061152444a1ea5d1
vincent <vrodomista@gmail.com>
2015-12-03 13:29:30 -0600

    Remove feed time from patient form

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -3,6 +3,4 @@ class Patient < ActiveRecord::Base
   has_many :stays
   belongs_to :feed_list
   validates :name, presence: true, length: { maximum: 50 }
-  validates :feed_time, :inclusion => { :in => %w(AM, PM, BID),
-  	:message => "not a valid feeding time" }
 end
