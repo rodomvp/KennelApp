a73103f23c36ed84b8bd5ea597499fa0407688f3
rgulle4 <rgulle4@lsu.edu>
2015-12-04 21:46:30 -0600

    Put patient's name on occupied run

--- app/models/runn.rb
+++ app/models/runn.rb
@@ -3,6 +3,25 @@ class Runn < ActiveRecord::Base
   belongs_to :ward
   has_many :stays
 
+  def current_stay
+    stays = Stay.where(runn: self) 
+    stays.last
+  end
+
+  def current_patient
+    stay = current_stay
+    if stay.nil?
+      return nil
+    else
+      return stay.patient
+    end
+  end
+
+  def current_patient_name
+    return nil if self.current_patient.nil?
+    return self.current_patient.name
+  end
+
   def self.get_empty_runns
   	empty_runns = [] 
   	Runn.all.each do |runn|
