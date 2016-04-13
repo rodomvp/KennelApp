7e97b23ad29e951a5250506522f407d4658b075e
vincent rodomista <vrodomista@f4-5c-89-8f-20-cb.wlan.lsu.edu>
2016-04-12 21:22:18 -0500

    fix log in mishap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   def login_required
+  	if (!User.find_by_id(session[:user_id]))
+  	redirect_to login_url 
   	flash[:danger] = "Please log in to access this page"
-    redirect_to login_url
+  	end
   end
   protect_from_forgery with: :exception
   include SessionsHelper
