1bdf0368add1ce9fbb8e4ae646f445fab4d84c91
vincent <vrodomista@gmail.com>
2016-04-13 18:43:10 -0500

    update authorization on users index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update, :create, :new]
+  before_action :logged_in_user, only: [:edit, :update, :index, :create, :new]
   def show
     @user = User.find(params[:id])
   end
