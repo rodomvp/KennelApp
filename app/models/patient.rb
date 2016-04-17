fe8bbb292bab420239e922674d56ce5799a64ae8
rgulle4 <rgulle4@lsu.edu>
2016-04-17 10:07:40 -0500

    STYLE: format app/models/patient.rb

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,7 +2,10 @@ class Patient < ActiveRecord::Base
   belongs_to :owner
   has_many :stays
   belongs_to :feed_list
-  validates :name, presence: true, length: { maximum: 50 }, uniqueness: {case_sensitive: false}
+  validates :name, 
+    presence: true, 
+    length: { maximum: 50 }, 
+    uniqueness: {case_sensitive: false}
 
   def has_current_stay?
     return false if stays.empty?
