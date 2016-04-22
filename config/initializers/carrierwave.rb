2fca6e4f405cc30bab899588dcbf3dca59e5d3f1
Vincent Rodomista <vrodomista@gmail.com>
2016-04-21 22:36:59 -0500

    i think i fixerinod

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,7 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => ENV["AWS_ACCESS_KEY_ID"],                        # required
-    :aws_secret_access_key  => ENV["S3_SECRET_ACCESS_KEY"]                         # required
+    :aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY"]                         # required
   }
   config.fog_directory  = ENV["AWS_BUCKET"]                     # required
 end
