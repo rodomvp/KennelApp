1d0d83cb2172122d1e1acb724ccd0f7261253eb5
Vincent Rodomista <vrodomista@gmail.com>
2016-04-25 17:00:11 -0500

    lets see

--- db/schema.rb
+++ db/schema.rb
@@ -63,7 +63,7 @@ ActiveRecord::Schema.define(version: 20160421005706) do
     t.boolean  "npo"
     t.string   "feed_time"
     t.integer  "feed_list_id"
-    t.boolean  "has_current_stay"
+    t.boolean  "has_current_stay",  null:false
     t.string   "image"
   end
 
